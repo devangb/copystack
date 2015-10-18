2380116eafbde53d3f9d94b9735268caebbd420a
Mukarr <mukarramtailor@outlook.com>
2015-10-19 04:18:00 +0530

    func.js would  contain the callback functions

---
+++ func.js
@@ -0,0 +1,9 @@
+/**
+ * Register a callback function with the commands api, which will be called when
+ * one of our registered commands is detected.
+ */
+
+chrome.commands.onCommand.addListener(function(command) {
+  
+  });
+});
